50ea0977731bfff6ceb72d02a0eafd204126b05c
David Changea <14ir155@gmail.com>
2024-03-30 13:02:21 +0100

    avancée sur la page project, map des projets et des titres ok

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -2,15 +2,24 @@ import { useState } from "react";
 import HeadPages from "../components/HeadPages";
 import { FaDev } from "react-icons/fa6";
 import dataFilter from "../data/filterProject.json";
+import dataProject from "../data/listProject.json";
 
 const ProjectPage = () => {
   const [filteredId, setFilteredId] = useState(0);
-  console.log(filteredId);
+  const [hoveredValue, setHoveredValue] = useState(null);
 
   function handleCurentId(currentId) {
     setFilteredId(currentId);
   }
 
+  function handleHover(index) {
+    setHoveredValue(index);
+  }
+
+  const filteredItems =
+    filteredId === 0
+      ? dataProject
+      : dataProject.filter((item) => item.filterId === filteredId);
   return (
     <main>
       <HeadPages headText="Mes projets" icon={<FaDev size={45} />} />
@@ -27,6 +36,21 @@ const ProjectPage = () => {
               </li>
             ))}
           </ul>
+          <div className="cards">
+            {filteredItems.map((item, index) => (
+              <div
+                className="cards_item"
+                key={`item + ${index}`}
+                onMouseEnter={() => handleHover(index)}
+                onMouseLeave={() => handleHover(null)}
+              >
+                <div className="cards_item_img-wrapper">
+                  <img src={item.cover} alt={`image du ${item.title}`} />
+                  <p>{item.title}</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </section>
     </main>
