8ad753fd7c0360f47803afce668b4511927a1434
David Changea <14ir155@gmail.com>
2024-04-03 00:12:59 +0200

    gestion de transition dans contacttext

--- src/components/ContactText.jsx
+++ src/components/ContactText.jsx
@@ -3,17 +3,22 @@ import data from "../data/textContact.json";
 
 const ContactText = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [fadeIn, setFadeIn] = useState(true);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex === 0 ? 1 : 0));
+      setFadeIn(false);
+      setTimeout(() => {
+        setCurrentIndex((prevIndex) => (prevIndex === 0 ? 1 : 0));
+        setFadeIn(true);
+      }, 500); // Attendre la fin de la transition avant de changer l'index
     }, 40000);
 
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <div className="contact_text">
+    <div className={`contact_text ${fadeIn ? "fade-in" : "fade-out"}`}>
       <h1>{data[currentIndex].title}</h1>
       <p className="text_top">{data[currentIndex].textTop}</p>
       <p className="text_bottom">{data[currentIndex].textBottom}</p>
