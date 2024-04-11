408d61e9ab466ec7405bc2972e8aa78d0f8b15b8
David Changea <14ir155@gmail.com>
2024-04-11 16:57:33 +0200

    page projet type version 1

--- src/pages/OneProjectPage.jsx
+++ src/pages/OneProjectPage.jsx
@@ -1,9 +1,11 @@
 import dataProject from "../data/listProject.json";
 import HeadPages from "../components/HeadPages";
 import { FaFileWaveform } from "react-icons/fa6";
+import { TbArrowBigRightLinesFilled } from "react-icons/tb";
 import { useParams } from "react-router-dom";
 import Caroussel from "../components/Caroussel";
 import ProjectTags from "../components/ProjectTags";
+import Button from "../components/Button";
 
 const OneProjectPage = () => {
   const { id } = useParams();
@@ -16,14 +18,61 @@ const OneProjectPage = () => {
         icon={<FaFileWaveform size={45} />}
       />
       <section className="one_project_page_section">
-        <h1>Illustrations du projet :</h1>
-
+        <h1>Illustrations du projet</h1>
         <Caroussel slides={data.pictures} />
-        <h2></h2>
-        <div className="tags">
-          {data.tags.map((tag, index) => (
-            <ProjectTags key={`tag ${index}`} text={tag} />
-          ))}
+        <h2>Technologies, méthodes et outils</h2>
+        <div className=" content_project one_project_page_section_content_tags">
+          <div className="tags">
+            {data.tags.map((tag, index) => (
+              <ProjectTags key={`tag ${index}`} text={tag} />
+            ))}
+          </div>
+        </div>
+        <h2>{`A propos de ${data.pageId}`}</h2>
+        <div className="content_project one_project_page_section_about_project">
+          <div className="one_project_page_section_about_project_content">
+            <TbArrowBigRightLinesFilled
+              size={28}
+              className="one_project_page_section_about_project_content_icon"
+            />
+            <p>{data.aboutText1}</p>
+          </div>
+          <div className="one_project_page_section_about_project_content">
+            <TbArrowBigRightLinesFilled
+              size={28}
+              className="one_project_page_section_about_project_content_icon"
+            />
+            <p>{data.aboutText2}</p>
+          </div>
+          <div className="one_project_page_section_about_project_content">
+            <TbArrowBigRightLinesFilled
+              size={28}
+              className="one_project_page_section_about_project_content_icon"
+            />
+            <p>{data.aboutText3}</p>
+          </div>
+        </div>
+        <div className="one_project_page_section_link">
+          <div className="one_project_page_section_link_button">
+            <TbArrowBigRightLinesFilled size={28} className="project_icon" />
+            <a href={data.github} target="_blank" rel="noopener noreferrer">
+              <Button
+                type={"submit"}
+                className={"button_project"}
+                text={"Vers GitHub"}
+              />
+            </a>
+          </div>
+          <div className="one_project_page_section_link_button">
+            <TbArrowBigRightLinesFilled size={28} className="project_icon" />
+            <a href={data.website} target="_blank" rel="noopener noreferrer">
+              <Button
+                type={"submit"}
+                className={"button_project "}
+                text={"Voir le site"}
+              />
+            </a>
+          </div>
         </div>
       </section>
     </main>
